b8e97c10306c15b5fbc662f1770d1055b6b8b3f1
Maks1232 <maksymilian.karasek@o2.pl>
2023-02-21 20:12:03 +0100

    Dane z lubelskiego

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 def main():
-    print('Hello World!')
+    lubelskie = {"Biała Podlaska": "LB",
+                 "Chełm": "LC",
+                 "Lublin": "LU",
+                 "Zamość": "LZ",
+                 "powiat bialski": "LBI",
+                 "powiat biłgorajski": "LBL",
+                 "powiat chełmski": "LLU",
+                 "powiat hrubieszowski": "LHR",
+                 "powiat janowski": "LJA",
+                 "powiat kraśnicki": "LKR",
+                 "powiat krasnostawski": "LKS",
+                 "powiat lubartowski": "LLB",
+                 "powiat lubelski": "LUB",
+                 "powiat opolski": "LOP",
+                 "powiat parczewski": "LPA",
+                 "powiat puławski":"LPU",
+                 "powiat radzyński": "LRA",
+                 "powiat rycki": "LRY",
+                 "powiat świdnicki": "LSW",
+                 "powiat tomaszowski": "LTM",
+                 "powiat włodawski": "LWL",
+                 "powiat zamojski": "LZA",
+                 "powiat łęczyński": "LLE",
+                 "powiat łukowski": "LLU"
+                 }
 
 
 if __name__ == '__main__':
     main()
-
